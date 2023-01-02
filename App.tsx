f13ddcb7125ac99ed1e109f0c0815b4b1479a9b5
Anemaygi <gianemayumisan@gmail.com>
2023-01-02 16:06:36 -0300

    adicionando

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { SignIn } from './src/interfaces/SignIn';
 import { Loading } from './src/components/Loading'
+import { Home } from './src/interfaces/Home';
 
 import { THEME } from './src/styles/theme'
 
--- src/interfaces/SignIn.tsx
+++ src/interfaces/SignIn.tsx
@@ -25,7 +25,9 @@ export function SignIn(){
         // VStack empilha verticalmente
         // Pq não consigo importar como <Logo />
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-                    
+
+             
+
              <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
              </Heading> 
